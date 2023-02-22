997832926e90b1e5aac53a5ca3f4d829aecc720a
Bryan Fernando <75253013+bryanfks-dev@users.noreply.github.com>
2023-02-23 01:26:17 +0700

    Update 50. Pow(x, n).c

--- 50. Pow(x, n).c
+++ 50. Pow(x, n).c
@@ -1,6 +1,8 @@
 double myPow(double x, int n){
+    // Use recursive method to find the power result
     if (x == 1 || n == 0) return 1;
-
+    
+    // Shorten power
     if (n % 2 == 0) {
         x *= x;
         n /= 2;
